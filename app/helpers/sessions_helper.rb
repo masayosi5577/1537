39f5dbef364c5df64e6f8ef0a0a6e8af5c5295dd
masayosi <dousoukai172000@gmail.com>
2020-05-06 08:51:51 +0000

    finish-10

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -29,7 +29,10 @@ module SessionsHelper
       end
     end
   end
-
+  
+  def current_user?(user)
+    user && user == current_user
+  end
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
@@ -41,4 +44,14 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end 
+  
+  def store_location
+    session[:forwarding_url]= request.original_url if request.get?
+  end
+  
 end
